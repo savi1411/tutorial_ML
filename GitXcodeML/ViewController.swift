7794e5ace664d5c2513ba9bd5ab02fbc3ade781e
Carlos Alberto Savi <carlos.savi@itau-unibanco.com.br>
2016-11-23 19:23:23 -0200

    Implementação Hello World

--- GitXcodeML/ViewController.swift
+++ GitXcodeML/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btnHelloWorld(_ sender: Any) {
+        
+        let alerta = UIAlertController(title: "Sample App", message: "Git Demonstration", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alerta.addAction(action)
+        present(alerta, animated: true, completion: nil)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
